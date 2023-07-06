f73d52110435c3c1f68909ce1e39caeb59d6d7d5
nathanLanar <nathan.simon@3wa.io>
2023-07-06 18:19:58 +0200

    whadevs

---
+++ lorga_react/src/Components/Accueil/Accueil.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import mc from "./acceuil.module.scss";
+import photoPda from "../../public/medias/photoPda.jpg";
+
+const Accueil = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Accueil;
---
+++ lorga_react/src/Components/Header/Header.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../public/medias/lorgaLogo.jpg";
+import mc from "./header.module.scss";
+import photoPda from "../../public/medias/photoPda.jpg";
+const Header = () => {
+  const [opacity, setOpacity] = useState(1);
+
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
+    const threshold = 300;
+
+    const newOpacity = 1 - scrollPosition / threshold;
+    const clampedOpacity = Math.max(0, Math.min(1, newOpacity));
+    setOpacity(clampedOpacity);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <header>
+      <div className={`${mc.headerPhoto}`} style={{ opacity }}>
+        <img
+          src={photoPda}
+          alt="Photo de la devanture du bar Lorganiq à bordeaux"
+        />
+      </div>
+      <div className={`${mc.blocLogo}`}>
+        <img src={logo} alt="Logo de Lorga" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
