5839386d067d458bc0a53be6b37a9f07e50bdc49
nathanLanar <nathan.simon@3wa.io>
2023-10-17 00:42:22 +0200

    try

--- lorga_react/src/index.js
+++ lorga_react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Accueil from "./components/Accueil/accueil";
+import Accueil from "./components/accueil/accueil";
 import Login from "./components/login/login";
 import SignUp from "./components/signUp/signUp";
 import Admin from "./admin/pageAdmin/admin";
